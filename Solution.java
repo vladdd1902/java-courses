4e72b1db6e735caa3ef5c2eda24ed5cb1b377e28
vladdd1902 <vlad_koval_96@mail.ru>
2015-11-25 16:15:33 +0200

    New prog

--- Solution.java
+++ Solution.java
@@ -1,11 +1,45 @@
+import java.lang.Math;
+
 public class Solution{
 
-public static void main(String[] arg)
+public static void main(String[] arg) throws Exception
 {
-System.out.println("Hello");
+
+double a = Double.parseDouble(arg[0]);
+String s = arg[1];
+double b = Double.parseDouble(arg[2]);
+
+switch(s)
+{
+
+case "+":
+System.out.println("Сумма 1-го и 2-го чисел ровна " + (a+b));
+break;
+
+case "-":
+System.out.println("Разница 1-го и 2-го чисел ровна " + (a-b));
+break;
+
+case "/":
+System.out.println("Деление 1-го на 2-го число ровно " + (a/b));
+break;
+
+case "^":
+System.out.println("Возведение 1-го числа в степень 2-го ровно " + Math.pow(a,b));
+break;
+
+case "!":
+System.out.println("Умножение 1-го на 2-го число ровно " + (a*b));
+break;
+
+default:
+System.out.println("Такой операции нету!");
+
 }
 
 
+
+}
 }
 
 
